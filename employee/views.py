52c75902aff68b9bf667c937fc9dd5d61659256c
Akelvinho <vinho892@gmail.com>
2025-12-08 00:52:42 +0300

    Finish creatig the addemployee feature and updattin the home view to be in a table format

--- employee/views.py
+++ employee/views.py
@@ -10,4 +10,9 @@ def home(request):
 
 def add_employee(request):
     form= EmployeeCreationForm()
+    if request.method == 'POST':
+        form = EmployeeCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     return render(request, 'employee/create_employee.html', {'form':form})
