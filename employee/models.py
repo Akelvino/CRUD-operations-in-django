57aca9e55b7d0e4e33108d625e416840f15f431b
Akelvinho <vinho892@gmail.com>
2025-12-07 23:14:00 +0300

    Created the view and models

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=100)
+    employee_department = models.CharField(max_length=100)
